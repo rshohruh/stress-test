b69e516cdf15ec3277c75236b4caaf6edff1cb58
Shohruh Rahmatillayev <rshohrux16@gmail.com>
2024-10-21 08:47:34 +0500

    fix shell path add template

---
+++ main.cpp
@@ -0,0 +1,30 @@
+// author: rshohruh
+
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+    #include "/home/rshohruh/debug.hpp"
+#else
+    #define debug(...) 42
+#endif
+using namespace std;
+using ll = long long;
+
+// #define with_testcases
+void t_main(){
+
+}
+
+signed main(){
+    signed t = 1;
+    cin.tie(nullptr)->sync_with_stdio(false);
+    #ifdef with_testcases
+        cin >> t;
+    #endif
+    while(t--){
+        t_main();
+        cout << '\n';
+    }
+    return 0;
+}
